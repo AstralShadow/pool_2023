731d66e55876d0ecca660dd98e934d54c0fd23b4
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-09-22 01:07:53 +0300

    game: adding mouse event handlers

---
+++ src/game/mouse.cpp
@@ -0,0 +1,20 @@
+#include "game.hpp"
+
+void game::
+mouseup(SDL_MouseButtonEvent& ev, scene_uid)
+{
+
+}
+
+void game::
+mousedown(SDL_MouseButtonEvent& ev, scene_uid)
+{
+
+}
+
+void game::
+mouse_motion(SDL_MouseMotionEvent& ev, scene_uid)
+{
+
+}
+
